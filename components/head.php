f6ea7d08c4c4463ff0e683b6aad7f867339dff9e
Miguel Soares Damin <miguelmiguelitoda@gmail.com>
2026-05-13 19:30:22 -0300

    UI: Added project logo as favicon

--- components/head.php
+++ components/head.php
@@ -11,6 +11,7 @@
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="default">
   <meta name="apple-mobile-web-app-title" content="Strively">
+  <link rel="icon" href="/images/icon-192.png" />
   <link rel="apple-touch-icon" href="/images/icon-192.png">
   <link rel="manifest" href="/manifest.json">
   
